418f322ac96a3f81bd96aee7467007efb2e799f7
egorka <egorka@tut.by>
2018-12-21 16:36:07 +0300

    service test start broken

--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,11 +1,14 @@
 package service
 
 import (
-	"github.com/egorka-gh/zbazar/zsync/repo"
+	"testing"
+
+	"github.com/egorka-gh/zbazar/zsync/pkg/repo"
 	"github.com/jmoiron/sqlx"
 )
 
-func newDb(cnn, folder string) (Repository, *sqlx.DB, error) {
+//NewDb creates new repro
+func NewDb(cnn, folder string) (Repository, *sqlx.DB, error) {
 	//"root:3411@tcp(127.0.0.1:3306)/pshdata"
 	var db *sqlx.DB
 	db, err := sqlx.Connect("mysql", cnn)
@@ -13,4 +16,13 @@ func newDb(cnn, folder string) (Repository, *sqlx.DB, error) {
 		return nil, nil, err
 	}
 	return repo.New(db, folder), db, nil
+}
+
+func TestFixVersionMaster(t *testing.T) {
+	var mrep Repository
+	mrep, mdb, err := test.NewDb("root:3411@tcp(127.0.0.1:3306)/pshdata", "D:\\Buffer\\zexch")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mdb.Close()
 }
